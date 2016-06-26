92780250c30f8deedbb59467097146b272c49ea1
hin101 <hineshpatel0@googlemail.com>
2016-06-26 17:07:52 +0100

    added mutual friendships code

--- app/models/user_friendship.rb
+++ app/models/user_friendship.rb
@@ -5,7 +5,7 @@ class UserFriendship < ActiveRecord::Base
   attr_accessible :user_id, :friend_id, :user, :friend, :state
 
   state_machine :state, initial: :pending do
-  	after_transition on: :accept, do: :send_acceptence_email
+  	after_transition on: :accept, do: [:send_acceptence_email, :accept_mutual_friendship!]
 
     state :requested
 
@@ -30,6 +30,16 @@ class UserFriendship < ActiveRecord::Base
 
   def send_acceptence_email
   	UserNotifier.friend_request_accepted(id).deliver
+  end
+
+  def mutual_friendship
+    self.class.where({user_id: friend_id, friend_id: user_id}).first
+  end
+
+  def accept_mutual_friendship!
+    # Grab the mutual friendship and update the state without using
+    # the state machine so as not to invoke callbacks.
+    mutual_friendship.update_attribute(:state, 'accepted')
   end
 
 end
